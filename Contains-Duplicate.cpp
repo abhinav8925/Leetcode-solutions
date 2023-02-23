49d12088334107d42e55166576af0e9c2b9a5202
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2023-02-23 21:14:18 +0530

    Create Contains-Duplicate.cpp

---
+++ Contains-Duplicate.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+       unordered_map<int,int> a;
+       for(auto &i:nums)
+       {
+           a[i]++;
+       }
+       
+       for(auto &x:a)
+       {
+           if(x.second>=2)
+               return true;
+       }
+       
+       return false;
+    }
+};
