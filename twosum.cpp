364f20eb1bd7fd08111737aaff113edefbecaf2d
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2023-04-02 01:23:19 +0530

    Update twosum.cpp

--- twosum.cpp
+++ twosum.cpp
@@ -1,21 +1,74 @@
+// Brute Force -> using nested loops
+// Time Complexity -> O(n2)
+// Space Complexity -> O(1)
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-    vector<int> b(2);
-   
-    for(int i=0;i<nums.size();i++)
-    {
-        for(int j=i+1;j<nums.size();j++)
+        int n=nums.size();
+
+        for(int i=0;i<n;i++)
         {
-        if(nums[i]+nums[j]==target)
+            for(int j=i+1;j<n;j++)
             {
-                b[0]=i;
-                b[1]=j;
-                break;
+                if(nums[i]+nums[j]==target)
+                {
+                    return {i,j};
+                }
             }
         }
+        return {-1,-1};
     }
-    return b;
-    }
-    
 };
+
+
+// Better Approach -> Using Hashmaps
+// Time Complexity -> O(n)
+// Space Complexity -> O(n)
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        map<int,int> mp;
+        for(int i=0;i<n;i++)
+        {
+            int num=nums[i];
+            if(mp.find(target-num)!=mp.end())
+            {
+                return {i,mp[target-num]};
+            }
+            else
+                mp[num]=i;
+        }
+        
+        return {-1,-1};
+    }
+};
+
+
+// optimized Approach -> Using Two pointers
+// Time Complexity -> O(nlogn)
+// Space Complexity -> O(1)
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n=nums.size();
+
+        vector<int> nums2=nums;
+        sort(nums.begin(),nums.end());
+
+        for(int i=0,j=n-1;i<j;)
+        {
+            if(nums[i]+nums[j]>target)
+                j--;
+            else if(nums[i]+nums[j]<target)
+                i++;
+            else
+                return {i,j};
+        }
+        return {-1,-1};
+    }
+};
+
