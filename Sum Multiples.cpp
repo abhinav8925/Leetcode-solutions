1a297f0e5abcfe0d022fc077af162e09144db716
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2023-04-24 22:58:02 +0530

    Create Sum Multiples.cpp

---
+++ Sum Multiples.cpp
@@ -0,0 +1,14 @@
+// Problem Link -> https://leetcode.com/contest/weekly-contest-342/problems/sum-multiples/
+
+class Solution {
+public:
+    int sumOfMultiples(int n) {
+        int cnt=0;
+        for(int i=1;i<=n;i++)
+        {
+            if(i%3==0 || i%5==0 || i%7==0)
+                    cnt+=i;
+        }
+        return cnt;
+    }
+};
