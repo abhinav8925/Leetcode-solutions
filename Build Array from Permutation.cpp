1ec50f243ee171e72e2bc05cee6856824d3aa8fb
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2023-05-07 01:32:11 +0530

    Update and rename cfvgbhnjmkl.cpp to Build Array from Permutation.cpp

---
+++ Build Array from Permutation.cpp
@@ -0,0 +1,13 @@
+// Problem Link -> https://leetcode.com/problems/build-array-from-permutation/description/
+
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        vector<int> v(nums.size());
+        for(int i=0;i<nums.size();i++)
+        {
+            v[i]=nums[nums[i]];
+        }
+        return v;
+    }
+};
