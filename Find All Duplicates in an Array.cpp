a000d91dbfb09402d3541c53217d4835bd10a56d
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2023-05-12 03:18:04 +0530

    Update and rename cfvbhjnk.cpp to Find All Duplicates in an Array.cpp

---
+++ Find All Duplicates in an Array.cpp
@@ -0,0 +1,88 @@
+// Problem Link -> https://leetcode.com/problems/find-all-duplicates-in-an-array/description/
+
+// Brute Force
+// class Solution {
+// public:
+//     vector<int> findDuplicates(vector<int>& nums) {
+        
+//         vector<int> v;
+//         map<int,int> m;
+        
+//         for(int i=0;i<nums.size();i++)
+//         {
+//            m[nums[i]]++;
+//         }
+//         for(auto &it :m)
+//         {
+//             if(it.second==2)
+//                 v.push_back(it.first);
+//         }
+//         return v;
+//     }
+// };
+
+
+// Better -> to use a hashmap 
+
+// better -> to use a 2 array
+
+// class Solution {
+// public:
+//     vector<int> findDuplicates(vector<int>& nums) {
+        
+//         vector<int> v;
+//         int n=nums.size();
+//         vector<int> b(n+1,0);
+//         for(int i=0;i<n;i++)
+//         {
+//             b[nums[i]]++;
+//         }
+//         for(int i=0;i<n+1;i++)
+//         {
+//             if(b[i]>1)
+//                 v.push_back(i);
+//         }
+//         return v;
+//     }
+// };
+
+// Optimal 
+
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        
+        vector<int> v;
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            int ind=abs(nums[i])-1;
+            if(nums[ind]<0)
+                v.push_back(abs(nums[ind]));
+            else
+                nums[ind]=-nums[ind];
+                
+        }
+        return v;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
