05c912fdc52643bf5e18353cebec833c032fbe68
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2023-08-16 20:57:31 +0530

    Create Find Minimum in Rotated Sorted Array.cpp

---
+++ Find Minimum in Rotated Sorted Array.cpp
@@ -0,0 +1,27 @@
+// Problem Link -> https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+
+class Solution {
+public:
+    int findMin(vector<int>& a) {
+
+            int n=a.size();
+            int l=0,h=n-1;
+            int ans=INT_MAX;
+            while(l<=h){
+
+                  int mid=(l+h)/2;
+                   if(a[l]<=a[mid]){
+                    ans=min(ans,a[l]);
+                    l=mid+1;
+                   }
+                   else{
+                       ans=min(ans,a[mid]);
+                       h=mid-1;
+                   }
+            }
+
+            return ans;
+
+        
+    }
+};
