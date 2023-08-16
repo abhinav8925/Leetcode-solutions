b1a2809e6b2f78c86bbbb318459e500d63cb91f2
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2023-08-16 20:58:56 +0530

    Update Missing Number.cpp

--- Missing Number.cpp
+++ Missing Number.cpp
@@ -1,6 +1,12 @@
+// Problem Link -> https://leetcode.com/problems/missing-number/
+
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
+        
+        /* 
+        brute force
+
         int val=0;
         sort(nums.begin(),nums.end());
         int i;
@@ -17,6 +23,17 @@ public:
         else
             return val;
 
+        */
+
+        // optimal 
+
+        int sum = 0; 
+        int total = nums.size()*(nums.size() + 1)/2; 
+        for (auto number : nums) { 
+            sum += number; 
+        }
+        return total - sum;
+        
 
     }
 };
