ba0bc71824f7628542521621f03fcef574bbee19
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2023-09-28 15:54:22 +0530

    Time: 4 ms (97.57%), Space: 16.9 MB (45.14%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,39 @@
+// Problem Link -> https://leetcode.com/problems/jump-game-ii/
+
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        
+        int n = nums.size();
+
+        if(n==1)
+            return 0;
+        
+        if(nums[0] == 0)
+            return 0;
+        
+        int jp = 1;
+        int sp = nums[0];
+        int rng = nums[0];
+
+
+        for(int i = 1;i<n;i++){
+
+            if(i == n-1)
+                return jp;
+            
+            rng = max(rng,i+nums[i]);
+
+            sp--;
+
+            if(sp == 0){
+                jp++;
+                if(rng<=i)
+                    return -1;
+                
+                sp = rng-i;
+            }
+        }
+        return jp;
+    }
+};
