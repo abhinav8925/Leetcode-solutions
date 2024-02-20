ad0995e95dfb2bfc8c788377c2ed27bb98e6eaad
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2024-02-21 01:20:56 +0530

    Time: 15 ms (58.85%) | Memory: 20.5 MB (19.44%) - LeetSync

---
+++ 268-missing-number/missing-number.cpp
@@ -0,0 +1,39 @@
+// Problem Link -> https://leetcode.com/problems/missing-number/
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        
+        /* 
+        brute force
+
+        int val=0;
+        sort(nums.begin(),nums.end());
+        int i;
+        for(i=0;i<nums.size();i++)
+        {
+            if(i!=nums[i])
+                {
+                    val=i;
+                    break;
+                }
+        }
+        if(i>=nums.size())
+        return i;
+        else
+            return val;
+
+        */
+
+        // optimal 
+
+        int sum = 0; 
+        int total = nums.size()*(nums.size() + 1)/2; 
+        for (auto number : nums) { 
+            sum += number; // 
+        }
+        return total - sum;
+        
+
+    }
+};
