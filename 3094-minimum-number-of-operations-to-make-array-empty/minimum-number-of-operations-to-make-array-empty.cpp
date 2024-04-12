0495513be2fc7ced6e904c7f820e9638b7572748
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2024-04-12 17:47:45 +0530

    Time: 126 ms (33.92%) | Memory: 94.4 MB (92.66%) - LeetSync

--- 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.cpp
+++ 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
-        map<int,int> m;
+        map<int,int> mp;
         int n = nums.size();
 
         for(int i=0;i<n;i++)
-            m[nums[i]]++;
-
-        int cnt=0;
-        for(auto it:m){
-            
-            int k = it.second;
-            if(k==1)
+            mp[nums[i]]++;
+        
+        int ans=0;
+        for(auto it:mp){
+            if(it.second<=1)
                 return -1;
-            else if(k%3==0)
-                cnt+=k/3;
-            else if(k%3==1)
-                cnt+=(k-1)/3+1;
+            if(it.second%3==0)
+                ans+=(it.second/3);
+            else if(it.second%3==1)
+                ans+=(it.second/3)-1 + 2;
             else
-                cnt+=k/3+1;
+                ans+=(it.second/3)  +1;
+                
         }
-
-        return cnt;
+        return ans;
     }
 };
