7fde6063f775f83d75a1acfce56a504e38b5e624
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2024-06-17 10:17:33 +0530

    Time: 2 ms (48.18%) | Memory: 10.4 MB (5.92%) - LeetSync

--- 413-arithmetic-slices/arithmetic-slices.cpp
+++ 413-arithmetic-slices/arithmetic-slices.cpp
@@ -1,29 +1,38 @@
-
 class Solution {
-public:
-    int numberOfArithmeticSlices(vector<int>& nums) {
-        if(nums.size() < 3)
-            return 0;
-        
-        int cnt = 0, diff = 0 , ind = 0;
-        int prev_diff = nums[1] - nums[0];
-        
-        for(int i = 1; i<nums.size()-1 ; ++i)
-        {
+    vector<vector<int>>memo;
 
-            int diff = nums[i+1] - nums[i];
-            if(diff ==  prev_diff)
-                ++ind;
-            
-            else
-            {
-                prev_diff = diff;
-                ind = 0; 
-            }
-                cnt += ind;
-        }
+public:
+    int helper(vector<int>&ar,int idx,bool takeIt,int prev,int diff){
         
-       
-        return cnt;
+    if((idx>=ar.size()-2 && !takeIt) || idx>=ar.size() || ar.size()<3){
+        return 0;
     }
+    
+    if( memo[idx][takeIt]!=-1){
+        return memo[idx][takeIt];
+    }
+     
+    if (takeIt){
+        if(ar[idx]-prev == diff){
+            return memo[idx][takeIt] = 1+ helper(ar,idx+1,1,ar[idx],diff);
+        } else return memo[idx][takeIt] = 0;
+    }
+        
+     int pick = 0;
+        
+     if(ar[idx+1]-ar[idx] == ar[idx+2]-ar[idx+1]){ // diff between to consictuve is  equal
+         pick = 1 + helper(ar,idx+3,1,ar[idx+2],ar[idx+1]-ar[idx]);
+     }
+	 
+    int leave =  helper(ar,idx+1,0,0,0);
+    
+	memo[idx][takeIt] = pick+leave; // memoization    
+    return pick+leave;
+}
+    
+int numberOfArithmeticSlices(vector<int>& nums) {
+     memo = vector<vector<int>>(nums.size(),vector<int>(2,-1));
+    return helper(nums,0,0,0,0);
+}
+
 };
