793c282ad73ea1a08e28c226667e106929ef26f1
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2025-10-09 23:17:37 +0530

    Time: 19 ms (7.00%) | Memory: 124.1 MB (11.40%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
@@ -1,3 +1,29 @@
+// Using the prefix sum method
+
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        
+        vector<double> p(nums.size(),0);
+        p[0] = nums[0];
+        for(int i=1;i<nums.size();i++){
+            p[i] = p[i-1] + nums[i];
+        }
+
+        double sum = p[k-1];
+        double mx = sum;
+
+        for(int i=1;i+k<=nums.size();i++){
+            sum = p[i+k-1]-p[i-1];
+            mx = max(mx,sum);
+        }
+
+        return mx/k;
+    }
+};
+
+
+/*
 class Solution {
 public:
     double findMaxAverage(vector<int>& nums, int k) {
@@ -15,6 +41,8 @@ public:
             mx = max(mx,sum);
         }
 
-        return max(mx,sum)/k;
+        return mx/k;
     }
 };
+
+*/
