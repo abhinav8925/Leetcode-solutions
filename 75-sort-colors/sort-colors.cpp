c52590eb51d08540242207ee574bc0fd278d93bb
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2026-05-02 23:00:43 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (77.67%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,30 +1,23 @@
-// Brute Force -> use sort inbuilt function
-// better -> Count sort
-// Optimal -> DNF
 class Solution {
 public:
-    void sortColors(vector<int>& a) {
-        int n=a.size();
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        int l=0,r=n-1,mid=0;
 
-        int low=0,mid=0,high=n-1;
-        while(mid<=high)
-        {
-            if(a[mid]==0)
-            {
-                swap(a[mid],a[low]);
-                low++;
+        while(mid<=r){
+            if(nums[mid] == 0){
+                swap(nums[mid],nums[l]);
+                l++;
                 mid++;
             }
-            else if(a[mid]==1)
-            {
+            else if(nums[mid] == 1){
                 mid++;
             }
-            else if(a[mid]==2)
-            {
-                swap(a[mid],a[high]);
-                high--;
+            else{
+                swap(nums[mid], nums[r]);
+                r--;
             }
+
         }
-
     }
 };
