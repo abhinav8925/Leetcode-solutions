f79407ebfff6fff451705d67e9233fc9babd9fff
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2026-05-03 00:08:01 +0530

    Time: 0 ms (100.00%) | Memory: 24 MB (18.61%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -4,9 +4,7 @@ public:
         int n = nums.size();
 
         int i=0,j=0;
-        while(i<n-1 && nums[i] == 0 && nums[i] == nums[i+1])
-            i++;
-        
+                
         for(;i<n;i++){
             if(nums[i]!=0){
                 swap(nums[j], nums[i]);
