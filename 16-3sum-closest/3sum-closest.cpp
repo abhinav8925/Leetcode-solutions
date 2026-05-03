83b8a08db046fbd586d838c3c45cc01d5d8fc4ec
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2026-05-03 23:32:33 +0530

    Time: 23 ms (7.98%) | Memory: 14.1 MB (33.27%) - LeetSync

--- 16-3sum-closest/3sum-closest.cpp
+++ 16-3sum-closest/3sum-closest.cpp
@@ -1,32 +1,37 @@
-// Problem Link -> https://leetcode.com/problems/3sum-closest/
-
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-    int n = nums.size();
-    int res =0,diff =INT_MAX;
-    sort(nums.begin(),nums.end());
-    for(int i =0;i<n-1;i++){
-    int l = i+1;
-    int r = n -1;
+        
+        sort(nums.begin(),nums.end());
 
-        while(l<r){
-            int sum = nums[i]+nums[l]+nums[r];
-            
-            if(abs(target-sum)<diff){
-                diff = abs(target-sum);
-                res = sum;
-            }
-            if(sum == target){ 
-                return target;
-            }
-            if(sum<target){
-                l++;
-            }
-            else r--;
-        } 
-    }
-    return res;
-    }
+        int n = nums.size(),r_ans = INT_MAX;
+        int ans = INT_MAX;
+    
+        for(int i=0;i<n-2;i++){
+            int l = i+1,r = n-1;
 
+            while(l<r){
+            int sum = nums[i] + nums[l] + nums[r];
+                if(sum == target){
+                    return sum;
+                }else if(sum > target)  r--;
+                else    l++;
+                
+                int diff = 0;
+                if((sum>=0 && target>=0) || (sum<=0 && target<=0))
+                    diff = abs(abs(sum) - abs(target));
+                else if(sum>=0 && target<0)
+                    diff = sum+abs(target);
+                else 
+                    diff = abs(sum) + target;
+                
+                ans = min(ans,diff);
+                if(ans == diff)
+                    r_ans = sum; 
+            }
+        }
+
+        return r_ans;
+
+    }
 };
