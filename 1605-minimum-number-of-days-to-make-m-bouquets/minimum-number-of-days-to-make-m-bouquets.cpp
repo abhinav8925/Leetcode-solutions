8ffec674a69a50fd2286c25b6083fa0647bd16fc
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2026-06-25 01:01:26 +0530

    Time: 16 ms (91.19%) | Memory: 70.4 MB (77.75%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,34 +1,49 @@
-// Problem Link -> https://leetcode.com/problems/minimum-number-of-days-to-make-m-bouquets/description/
-
 class Solution {
 public:
-    int getBouq(vector<int>& bloomDay,int day,int k)	
-    {
-        int result=0,count=0;
-        for(int i=0;i<bloomDay.size();i++)
-        {
-            if(bloomDay[i]<=day)
-                count++;
-            else
-                count=0;
-            if(count==k)    
-                result++,count=0;
+
+    bool f(int mid, vector<int> &nums, int m, int k){
+
+        int n = nums.size();
+        int temp=0, cnt=0;
+
+        for(int i=0;i<n;i++){
+            if(cnt >= m) return true;
+            
+            if(nums[i]<=mid){
+                temp++;
+            }else{
+                temp=0;
+            }
+            if(temp == k){
+                    cnt++;
+                    temp=0;
+            } 
         }
-        return result;
+
+        return cnt>=m ? true : false;
     }
-    int minDays(vector<int>& bloomDay, int m, int k) 
-    {
-        auto p=minmax_element(bloomDay.begin(),bloomDay.end());
-        int j=*p.second,i=*p.first;
-        int mid;
-        while(i<j)				
-        {
-            mid=(i+j)>>1;
-            if(getBouq(bloomDay,mid,k)<m)
-                i=mid+1;
-            else
-                j=mid;
+    int minDays(vector<int>& nums, int m, int k) {
+        
+
+        int n = nums.size();
+        if((long)m*k > n) return -1;
+
+        int low = *min_element(nums.begin(),nums.end());
+        int high = *max_element(nums.begin(),nums.end());
+
+        int ans = -1;
+
+        while(low<=high){
+
+            int mid = low + (high-low)/2;
+            if(f(mid, nums, m,k)){
+                ans = mid;
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
         }
-        return getBouq(bloomDay,i,k)>=m?i:-1;		
+
+        return ans;
     }
 };
