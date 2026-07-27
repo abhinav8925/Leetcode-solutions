6bed5bee0ffbc8d7f244957e105aaa58e5fc3f06
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2026-07-28 01:17:18 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (72.06%) - LeetSync

--- 684-redundant-connection/redundant-connection.cpp
+++ 684-redundant-connection/redundant-connection.cpp
@@ -1,21 +1,59 @@
+class DSU{
+    public: 
+
+    vector<int> parent,size;
+
+    DSU(int n){
+        parent.resize(n);
+        size.resize(n,1);
+
+        for(int i=0;i<n;i++)    
+            parent[i] = i;
+
+    }
+
+    int findParent(int i){
+        if(parent[i] == i)  
+            return i;
+
+        return parent[i] = findParent(parent[i]);
+    }
+
+    bool unionBySize(int p, int q){
+        int a = findParent(p);
+        int b = findParent(q);
+
+        if(a == b)  return false;
+
+        if(size[a] > size[b]){
+            parent[b] = a;
+            size[a] += size[b];
+        }else{
+            parent[a] = b;
+            size[b] += size[a];
+        }
+
+        return true;
+    }
+};
+
 class Solution {
 public:
-    vector<int> findRedundantConnection(vector<vector<int>>& e) {
-	int n = size(e);
-	vector<vector<int>> graph(n+1);
-	vector<bool> vis(n+1);                
-	for(auto& E : e) {
-        fill(begin(vis), end(vis), false);     
-        graph[E[0]].push_back(E[1]), graph[E[1]].push_back(E[0]);
-        if(dfs(graph, vis, E[0])) return E;
+    vector<int> findRedundantConnection(vector<vector<int>>& pre) {
+        
+
+        int n = pre.size();
+        DSU DSU1(n+1);
+
+        vector<int> ans;
+        for(int i=0;i<pre.size();i++){
+            int a = pre[i][0];
+            int b = pre[i][1];
+
+            if(!(DSU1.unionBySize(a,b))){
+                ans = {a,b};
+            }
+        }
+        return ans;
     }
-	return { };    
-}
-bool dfs(vector<vector<int>>& graph, vector<bool>& vis, int cur, int par = -1) {
-    if(vis[cur]) return true;   
-    vis[cur] = true;
-    for(auto child : graph[cur]) 
-        if(child != par && dfs(graph, vis, child, cur)) return true;
-    return false;    
-}
 };
