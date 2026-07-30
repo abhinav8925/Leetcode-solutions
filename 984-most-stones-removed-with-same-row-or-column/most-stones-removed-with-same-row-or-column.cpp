b41a3c01b37012f6bf913ee1c0b89ca45d3c688a
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2026-07-31 01:24:55 +0530

    Time: 63 ms (16.74%) | Memory: 36 MB (5.26%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
@@ -1,20 +1,71 @@
+class DSU{
+    public: 
+        map<int,int> parent;
+        vector<int> size;
+
+        DSU(int n){
+            size.resize(n,1);
+            
+            for(int i=0;i<n;i++){
+                parent[i]=i;
+            
+            }
+        }
+
+        int findParent(int n){
+            if(parent[n] == n)  return n;
+            return parent[n] = findParent(parent[n]);
+        }
+
+        void un(int p, int q){
+            int a = findParent(p);
+            int b = findParent(q);
+
+            if(a==b)    return;
+            if(size[a] >= size[b]){
+                parent[b] = a;
+                size[a] += size[b]; 
+            }else{
+                parent[a] = b;
+                size[b] += size[a];
+            }
+        }
+};
+
+
 class Solution {
 public:
-    int dfs(unordered_map<int, vector<int>> &rows, unordered_map<int, vector<int>> &cols, unordered_set<int> &v_rows, int row) {
-  auto res = 0;
-  if (v_rows.insert(row).second) {
-    res += rows[row].size();
-    for (auto c : rows[row])
-      for (auto r : cols[c]) res += dfs(rows, cols, v_rows, r);
-  }
-  return res;
-}
-int removeStones(vector<vector<int>>& stones) {
-  unordered_map<int, vector<int>> rows, cols;
-  unordered_set<int> v_rows;
-  auto res = 0;
-  for (auto s : stones) rows[s[0]].push_back(s[1]), cols[s[1]].push_back(s[0]);
-  for (auto r : rows) res += max(0, dfs(rows, cols, v_rows, r.first) - 1);
-  return res;
-}
+    int removeStones(vector<vector<int>>& pre) {
+
+        int m = pre.size();
+        int mr=INT_MIN,mc=INT_MIN;
+        for(int i=0;i<m;i++){
+            mr = max(mr,pre[i][0]);
+            mc = max(mc,pre[i][1]);
+        }
+
+        DSU DSU1(mr+mc+2);
+
+        for(int i=0;i<m;i++){
+            int a = pre[i][0];
+            int b = pre[i][1];
+            DSU1.un(a,mr+b+1);
+        }
+
+        
+        set<int> s;
+        for(int i=0;i<m;i++){
+            int a = pre[i][0];
+            int b = pre[i][1];
+            if(DSU1.findParent(a)==a)
+            s.insert(a);
+            if( DSU1.findParent(b+mr+1) == b+mr+1)   
+            s.insert(b);
+         
+        }
+
+        
+        return m-s.size();
+        
+    }
 };
