71486523f828ca47b750536872b4fb5175761827
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2026-08-12 00:23:33 +0530

    Time: 58 ms (5.05%) | Memory: 88.6 MB (15.40%) - LeetSync

--- 525-contiguous-array/contiguous-array.cpp
+++ 525-contiguous-array/contiguous-array.cpp
@@ -4,20 +4,22 @@ public:
         
         int n = nums.size();
         map<int,int> mp;
-        int sum=0;
-        int ans=0;
-        mp[0]=0;
+        int sum=0,ans=0;
+        mp[0] = -1;
+
+        int cz=0,co=0;
+
         for(int i=0;i<n;i++){
-            if(nums[i]==0)
-                sum--;
+            if(nums[i] == 0)    cz+=1;
+            else                co+=1;
+
+            if(mp.find(co-cz) != mp.end()){
+                ans = max(ans,i-mp[co-cz]);
+            }
             else
-                sum++;
-            
-            if(mp.find(sum)!=mp.end())
-                ans = max(ans,i-mp[sum]+1);
-            else
-                mp[sum]=i+1;
+                mp[co-cz] = i;
         }
+
         return ans;
     }
 };
