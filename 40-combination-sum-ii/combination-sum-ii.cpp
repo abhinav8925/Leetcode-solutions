8cc2cb20fa19ba1ec2f707eaab42cac70ce70d02
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2026-09-02 00:44:58 +0530

    Time: 4 ms (21.92%) | Memory: 17.3 MB (7.94%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,41 +1,30 @@
-// Problem Link -> https://leetcode.com/problems/combination-sum-ii/description/
-
 class Solution {
 public:
-    void comb(int ind, int n, int target, vector<int> &ds, vector<int> &a, vector<vector<int>> &ans) {
-    
-        if (target == 0 ) {
-            ans.push_back(ds);
+    void f(int ind, vector<int> arr, int target, vector<vector<int>> &ans, vector<int> &temp){
+
+        if(target == 0){
+            ans.push_back(temp);
             return;
         }
-    
-    
 
-    for(int i=ind;i<n;i++){
-        if(i>ind && a[i]==a[i-1])
-            continue;
-        
-        if(a[i]>target)
-            break;
-        
-        ds.push_back(a[i]);
-        comb(i+1, n, target - a[i], ds, a, ans);
-        ds.pop_back();
+        for(int i=ind;i<arr.size();i++){
+            if(i>ind && arr[i] == arr[i-1])
+                continue;
+            
+            if(arr[i] > target)
+                break;
+            
+            temp.push_back(arr[i]);
+            f(i+1,arr,target-arr[i],ans,temp);
+            temp.pop_back();
+        }
     }
-    
-}
-
-vector<vector<int>> combinationSum2(vector<int>& a, int target) {
-    int n = a.size();
-    vector<int> ds;
-
-    sort(a.begin(),a.end());
-    vector<vector<int>> ans;
-    
-
-    comb(0, n, target, ds, a, ans);
-
-    return ans;
-}
-
+    vector<vector<int>> combinationSum2(vector<int>& arr, int target) {
+        
+        vector<vector<int>> ans;
+        sort(arr.begin(),arr.end());
+        vector<int> temp;
+        f(0,arr,target,ans,temp);
+        return ans;
+    }
 };
